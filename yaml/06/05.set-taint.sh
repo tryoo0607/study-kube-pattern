6735c1e109809e620f0a816ab63159e27fa6dde3
tryoo0607 <tryoo0607@gmail.com>
2025-08-30 17:38:54 +0900

    Taint / Toleration 예제 수정

--- yaml/06/05.set-taint.sh
+++ yaml/06/05.set-taint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 kubectl taint nodes kind-worker dedicated=NoSchedule:NoSchedule
-kubectl taint nodes kind-worker4 dedicated=NoExcute:NoSchedule
+kubectl taint nodes kind-worker4 dedicated=NoExcute:NoExcute
